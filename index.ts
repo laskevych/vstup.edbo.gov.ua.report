17432299c86ca8fee1d93411b5fca8b8a992702e
Andrew Laskvych <a.laskevych@owox.com>
2022-08-09 19:16:20 +0300

    [master] Update logic

--- index.ts
+++ index.ts
@@ -1,61 +1,77 @@
-"use strict";
-var _a, _b;
-var PRIORITY;
-(function (PRIORITY) {
-    PRIORITY[PRIORITY["Error"] = -1] = "Error";
-    PRIORITY[PRIORITY["First"] = 1] = "First";
-    PRIORITY[PRIORITY["Second"] = 2] = "Second";
-    PRIORITY[PRIORITY["Third"] = 3] = "Third";
-    PRIORITY[PRIORITY["Fourth"] = 4] = "Fourth";
-    PRIORITY[PRIORITY["Fifth"] = 5] = "Fifth";
-    PRIORITY[PRIORITY["Contract"] = 6] = "Contract";
-})(PRIORITY || (PRIORITY = {}));
-var SCORE_METRIC;
-(function (SCORE_METRIC) {
-    SCORE_METRIC[SCORE_METRIC["min"] = 0] = "min";
-    SCORE_METRIC[SCORE_METRIC["avg"] = 1] = "avg";
-    SCORE_METRIC[SCORE_METRIC["max"] = 2] = "max";
-})(SCORE_METRIC || (SCORE_METRIC = {}));
+interface Applicant {
+    id: string,
+    priority: number,
+    score: number,
+    hasQuota: boolean
+}
+
+enum PRIORITY {
+    'Error' = -1,
+    'First'= 1,
+    'Second',
+    'Third',
+    'Fourth',
+    'Fifth',
+    'Contract'
+}
+
+enum SCORE_METRIC {
+    'min',
+    'avg',
+    'max'
+}
+
 const QUOTAS_LIMIT = 52;
 const TOTAL_LIMIT = 130;
 const CONTRACT_LIMIT = 20;
+
 class Index {
+    protected applicants: Applicant[];
+
     constructor() {
         this.applicants = this.extract();
     }
-    getApplicantsByFilter(priority, hasQuota) {
+
+    public getApplicantsByFilter(priority: PRIORITY | null, hasQuota: boolean | null): Applicant[] {
         return this.getApplicants().filter(applicant => {
+
             if (priority && hasQuota === null) {
                 if (applicant.priority === priority) {
                     return applicant;
                 }
-            }
-            else if (priority === null && hasQuota) {
+            } else if (priority === null && hasQuota) {
                 if (applicant.hasQuota === hasQuota) {
                     return applicant;
                 }
-            }
-            else if (priority !== null && hasQuota !== null) {
-                if (applicant.priority === priority
-                    && applicant.hasQuota === hasQuota) {
+            } else if (priority !== null && hasQuota !== null) {
+                if (
+                    applicant.priority === priority
+                    && applicant.hasQuota === hasQuota
+                ) {
                     return applicant;
                 }
-            }
-            else {
+            } else {
                 return applicant;
             }
+
         });
     }
-    getApplicants() {
+
+    public getApplicants(): Applicant[] {
         let applicants = this.applicants;
+
         applicants.sort(this.sortByScore);
+
         return applicants;
     }
-    getScoreMetric(applicants, metric) {
+
+    public getScoreMetric(applicants: Applicant[], metric: SCORE_METRIC): number {
+
         if (applicants.length === 0) {
             throw new Error('Applicants must be > 0.');
         }
-        let metricValue = 0;
+        let metricValue: number = 0;
+
         switch (metric) {
             case SCORE_METRIC.min:
                 applicants.sort(this.sortByScore);
@@ -66,80 +82,103 @@ class Index {
                 metricValue = applicants[0].score;
                 break;
             case SCORE_METRIC.avg:
-                let avgScore = 0;
+                let avgScore: number = 0;
                 applicants.forEach(applicant => {
                     avgScore += applicant.score;
                 });
+
                 metricValue = avgScore / applicants.length;
                 break;
             default:
-                new Error(`${metric} not found in SCORE_METRIC.`);
+                new Error(`${metric} not found in SCORE_METRIC.`)
         }
+
         return metricValue;
     }
-    sortByScore(a, b) {
+
+    public sortByScore(a: Applicant, b: Applicant): number {
         return a.score > b.score ? -1 : 1;
     }
-    extract() {
+
+    protected extract(): Applicant[] {
+
         /**
          * Заявки со статусом "Допущенные"
          */
-        const STATUS_ADMITTED = 'request-status-6';
+        const STATUS_ADMITTED: string = 'request-status-6';
+
         const elements = document.getElementsByClassName(STATUS_ADMITTED);
+
         const applicants = [];
         for (let i = 0; i < elements.length; i++) {
             let element = elements[i];
+
             /* Id */
-            let id = '(not set)';
+            let id: string = '(not set)';
             let idElement = element.getElementsByClassName('offer-request-fio');
             if (idElement.length > 0) {
                 let textContent = idElement[0].getElementsByTagName('div')[0].textContent;
+
                 id = textContent ? textContent : '(not set)';
             }
+
             /* Score */
-            let score = -1;
+            let score: number = -1;
             let scoreElement = element.getElementsByClassName('offer-request-kv');
             if (scoreElement.length > 0) {
                 let textContent = scoreElement[0].getElementsByTagName('div')[0].textContent;
+
                 score = textContent ? parseInt(textContent) : -1;
             }
+
             /* Priority */
-            let priority = PRIORITY.Error;
+            let priority: PRIORITY = PRIORITY.Error;
             let priorityElement = element.getElementsByClassName('offer-request-priority');
             if (priorityElement.length > 0) {
+
                 let paidEducation = priorityElement[0].getElementsByClassName('offer-request-contract');
                 let freeEducation = priorityElement[0].getElementsByClassName('offer-request-other');
+
                 if (paidEducation.length > 0) {
                     priority = PRIORITY.Contract;
-                }
-                else if (freeEducation.length > 0) {
+                } else if (freeEducation.length > 0) {
                     let textContent = freeEducation[0].textContent;
+
                     priority = textContent ? parseInt(textContent) : PRIORITY.Error;
                 }
             }
+
             /* Quota */
-            let quota = false;
+            let quota: boolean = false;
             let quotaElement = element.getElementsByClassName('indicator-q');
             if (quotaElement.length > 0) {
                 quota = true;
             }
-            let applicant = {
+
+            let applicant: Applicant = {
                 id: id,
                 priority: priority,
                 score: score,
                 hasQuota: quota
-            };
-            if (applicant.score === PRIORITY.Error || applicant.priority < 0) {
-                new Error(`Error in data. Item: ${applicant}.`);
             }
+
+            if (applicant.score === PRIORITY.Error || applicant.priority < 0) {
+                new Error(`Error in data. Item: ${applicant}.`)
+            }
+
             applicants.push(applicant);
         }
+
         applicants.sort(this.sortByScore);
+
         return applicants;
     }
 }
+
 const extractor = new Index();
+
 /* Report */
+
 console.log(`🚀️ Priority`);
 console.log(`   1️⃣ Priority: ${extractor.getApplicantsByFilter(PRIORITY.First, null).length} requests.`);
 console.log(`   2️⃣ Priority: ${extractor.getApplicantsByFilter(PRIORITY.Second, null).length} requests.`);
@@ -149,49 +188,82 @@ console.log(`   5️⃣ Priority: ${extractor.getApplicantsByFilter(PRIORITY.Fif
 console.log(`   💵 Contract: ${extractor.getApplicantsByFilter(PRIORITY.Contract, null).length} requests.`);
 console.log(`   ⛔️ Error: ${extractor.getApplicantsByFilter(PRIORITY.Error, null).length} requests.`);
 console.log('\n');
+
 console.log(`👋 Quota: ${extractor.getApplicantsByFilter(null, true).length} requests of limit ${QUOTAS_LIMIT}`);
 console.log(`\n`);
+
 console.log(`1️⃣ First priority score report`);
 let applicantsFirstPriority = extractor.getApplicantsByFilter(PRIORITY.First, null);
-console.log(`    MAX: ${extractor.getScoreMetric(applicantsFirstPriority, SCORE_METRIC.max)}`);
-console.log(`    AVG: ${extractor.getScoreMetric(applicantsFirstPriority, SCORE_METRIC.avg)}`);
-console.log(`    MIN: ${extractor.getScoreMetric(applicantsFirstPriority, SCORE_METRIC.min)}`);
+console.log(`    MAX: ${extractor.getScoreMetric(
+    applicantsFirstPriority, 
+    SCORE_METRIC.max 
+)}`);
+
+console.log(`    AVG: ${extractor.getScoreMetric(
+    applicantsFirstPriority,
+    SCORE_METRIC.avg
+)}`);
+
+console.log(`    MIN: ${extractor.getScoreMetric(
+    applicantsFirstPriority,
+    SCORE_METRIC.min
+)}`);
 console.log(`\n`);
+
 console.log(`💵 Contract score report`);
 let applicantsContract = extractor.getApplicantsByFilter(PRIORITY.Contract, null);
-console.log(`    MAX: ${extractor.getScoreMetric(applicantsContract, SCORE_METRIC.max)}`);
-console.log(`    AVG: ${extractor.getScoreMetric(applicantsContract, SCORE_METRIC.avg)}`);
-console.log(`    MIN: ${extractor.getScoreMetric(applicantsContract, SCORE_METRIC.min)}`);
+console.log(`    MAX: ${extractor.getScoreMetric(
+    applicantsContract,
+    SCORE_METRIC.max
+)}`);
+
+console.log(`    AVG: ${extractor.getScoreMetric(
+    applicantsContract,
+    SCORE_METRIC.avg
+)}`);
+
+console.log(`    MIN: ${extractor.getScoreMetric(
+    applicantsContract,
+    SCORE_METRIC.min
+)}`);
 console.log(`\n`);
+
 console.log(`👑 Forecasted rating`);
 console.log(`\n`);
+
 let applicantsFirstPriorityQuotas = extractor.getApplicantsByFilter(PRIORITY.First, true);
 let applicantsFirstPriorityQuotasPass = applicantsFirstPriorityQuotas.slice(0, QUOTAS_LIMIT);
 let applicantsFirstPriorityQuotasNotPass = applicantsFirstPriorityQuotas.slice(QUOTAS_LIMIT + 1);
+
 let applicantsFirstPriorityWithoutQuotas = extractor.getApplicantsByFilter(PRIORITY.First, false);
-let applicantsOther = applicantsFirstPriorityWithoutQuotas.concat(applicantsFirstPriorityQuotasNotPass);
+let applicantsOther =  applicantsFirstPriorityWithoutQuotas.concat(applicantsFirstPriorityQuotasNotPass);
 applicantsOther.sort(extractor.sortByScore);
+
+
 applicantsFirstPriorityQuotasPass.forEach((applicant, index) => {
     console.log(`${index + 1}. ID: ${applicant.id} || ${applicant.score} ${applicant.hasQuota ? '|| 🎫' : ''}`);
 });
+
 for (let i = 0; i < TOTAL_LIMIT - QUOTAS_LIMIT; i++) {
-    let applicant = (_a = applicantsOther[i]) !== null && _a !== void 0 ? _a : undefined;
+    let applicant = applicantsOther[i] ?? undefined;
+
     if (applicant) {
         console.log(`${i + QUOTAS_LIMIT + 1}. ID: ${applicant.id} || ${applicant.score} ${applicant.hasQuota ? '|| 🎫' : ''}`);
-    }
-    else {
+    } else {
         console.log(`${i + QUOTAS_LIMIT + 1}. Free place ✅`);
     }
 }
 console.log(`\n`);
+
 console.log(`👑 Forecasted rating [contract]`);
 console.log(`\n`);
 for (let i = 0; i < CONTRACT_LIMIT; i++) {
-    let applicant = (_b = applicantsContract[i]) !== null && _b !== void 0 ? _b : undefined;
+    let applicant = applicantsContract[i] ?? undefined;
+
     if (applicant) {
         console.log(`${i + 1}. ID: ${applicant.id} || ${applicant.score}`);
-    }
-    else {
+    } else {
         console.log(`${i + 1}. Free place ✅`);
     }
 }
+
